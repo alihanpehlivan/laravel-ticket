b0d13e7b7513084d2f30a39570913ce8539d6264
ahmetcelikezer <ahmetcelikezer@icloud.com>
2019-01-07 03:59:34 +0300

    Initial Commit

---
+++ src/TicketServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Laravel Ticket Project
+ * An Open-Source ticket support system for laravel.
+ * License : MIT
+ * Authors : Alihan Pehlivan - alihan.pehlivan@students.plymouth.ac.uk / Ahmet Celikezer ahmet@ahmetcelikezer.com
+ * Version : 0.0.1
+ */
+namespace Liveth\laravelTicket;
+
+use Carbon\Laravel\ServiceProvider;
+
+
+class TicketServiceProvider extends ServiceProvider {
+     
+    public function boot() {
+        //
+    }
+
+    public function register() {
+        //
+    }
+}
